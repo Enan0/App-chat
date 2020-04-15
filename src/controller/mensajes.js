307da223243cd9acb531d44bef832675e1fe5ff6
Ezequiel Enano <zekikpoin@gmail.com>
2020-04-15 12:54:15 -0300

    Creamos los metodos viewAllUsers y createMensaje

--- src/controller/mensajes.js
+++ src/controller/mensajes.js
@@ -1,46 +1,18 @@
 const mensajeModel = require('../model/mensajes');
 const mensajeController = {}
 
-mensajeController.createMensaje = async(req,res)=>{
-    /*Guardamos los datos */
-    const {texto,de,para} = req.body;
-    const mensajeTmp = new mensajeModel({
-        de,
-        para,
+mensajeController.viewAllMensajes = async (req, res) => {
+    const mensajes = await mensajeModel.find();
+    res.json(mensajes);
+}
+
+
+mensajeController.createMensaje = (req, res) => { 
+    const {texto} = req.body;
+    const mensaje = new mensajeModel({
         texto
     });
-    await mensajeTmp.save()
-    .then(res.json({succes:"mensaje enviado"}));
-}
-
-mensajeController.readMensajes = async(req,res)=>{
-    /*Vemos los mensajes del chat */
+    mensaje.save();
     
-    //Obtenemos los integrantes del chat
-    const {para} = req.params;
-    const de = 'ezequiel';
-    //Guardamos los mensajes enviados y recibidos
-        const misMensajesEnviados = await mensajeModel.find({
-            de,
-            para
-        }) || "ninguno";
-        const misMensajesRecibidos = await mensajeModel.find({
-            de:para,
-            para:de
-        }) || "ninguno";
-    let mensaje = misMensajesRecibidos.concat(misMensajesEnviados);
-    mensaje.sort((a,b)=>{
-        if(a.createdAt < b.createdAt){
-            return a;
-        }else{
-            return b;
-        }
-
-    })
-    res.json({
-        mensajes:mensaje
-    });
 }
-
-
 module.exports = mensajeController;
