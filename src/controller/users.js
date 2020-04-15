96f6c637016ca8ee4032e05feef5be536d74540f
Ezequiel Enano <zekikpoin@gmail.com>
2020-04-15 12:47:12 -0300

    Cambios al metodo create user

--- src/controller/users.js
+++ src/controller/users.js
@@ -3,18 +3,29 @@ const userModel = require('../model/users');
 const userController = {}
 
 
-userController.viewAllUsers = (req,res)=>{
+//DEV TOOL
+userController.viewAllUsers = async (req, res) => {
     /*Muestra todos los usuarios */
     const users = await userModel.find();
     res.json(users);
 }
 
-userController.createUser = (req,res)=>{
-    /*Crea un usuario */ 
-    const {nombre} = req.body;
-    const user = new userModel({nombre})
-    user.save();
-    res.json({status:"user created"})
+userController.createUser = async (req, res) => {
+    /*Crea un usuario */
+    //1)Pedimos los datos 
+    const { nombre } = req.body;
+    //2)Creamos el usuario
+    const user = new userModel({ nombre });
+
+    //3)Lo guardamos
+    user.save()
+        .then(res.json({ status: "user created" }))
+        .catch((err) => {
+            res.json({
+                status: "error",
+                erro: err.message
+            });
+        });
 }
 
 
