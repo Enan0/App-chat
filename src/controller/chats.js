01c61ee86f34b2f2c2c8e45c38b2f274cc6d66d6
Ezequiel Enano <zekikpoin@gmail.com>
2020-04-15 13:28:37 -0300

    Ahora se puede pasar una lista de nombres por body y agregarlos al chat cuando lo creamos

--- src/controller/chats.js
+++ src/controller/chats.js
@@ -5,17 +5,20 @@ const chatController = {};
 
 chatController.createChat = async (req, res) => {
     /* Creamos un chat */
-    //1) Creamos los participantes *
-    const participante1 = await userModel.findOne({nombre:"ezequiel"});
-    const participante2 = await userModel.findOne({nombre:"juan"});
-    const participantes = [{participante:participante1},{participante:participante2}];
+    //1) Buscamos los participantes *
+        const {nombres} = req.body; //<-Obtiene una lista de nombres del form
+        const obtaniedUsers = await userModel.find(nombres); //<-busca en la db registros con los nombres
+
     // 2)Creamos el chat
-    const chat = new chatModel({
-        participantes: participantes
+        const chat = new chatModel();
+    //Recorre la lista de usuarios obtenidos y los agrega al chat
+    obtaniedUsers.map(usuario =>{
+        chat.participantes.push({participante:usuario});
     });
     //3) Lo persistimos
     chat.save()
         .then(()=>{res.json({ status: "true" })})
+        //Mensaje de error general
         .catch((err) => {
             res.json({
                 status: "error",
@@ -23,6 +26,7 @@ chatController.createChat = async (req, res) => {
             });
         });
 }
+
 //DEV TOOL
 chatController.viewAllChats = async (req, res) => {
     /*Vemos los chats */
