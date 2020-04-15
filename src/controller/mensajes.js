ebf15222402df5318b5e794d48fbbfcd230c3ee4
Ezequiel Enano <zekikpoin@gmail.com>
2020-04-15 13:09:12 -0300

    Agregamos un mensaje de error general en createMensaje

--- src/controller/mensajes.js
+++ src/controller/mensajes.js
@@ -1,18 +1,26 @@
 const mensajeModel = require('../model/mensajes');
 const mensajeController = {}
 
+//DEV TOOL
 mensajeController.viewAllMensajes = async (req, res) => {
+    /*Vemos todos los mensajes */
     const mensajes = await mensajeModel.find();
     res.json(mensajes);
 }
 
 
-mensajeController.createMensaje = (req, res) => { 
+mensajeController.createMensaje = async(req, res) => { 
+    /*Crea un mensaje */
     const {texto} = req.body;
     const mensaje = new mensajeModel({
         texto
     });
-    mensaje.save();
+    await mensaje.save()
+    .then(res.json({status:"mensaje creado"}))
+    .catch((err)=>{
+        res.json({ERROR:err.message});
+    })    
+    
     
 }
 module.exports = mensajeController;
