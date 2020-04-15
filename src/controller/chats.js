f273fb48466beb8b9c7f6feda0800eed8f18f503
Ezequiel Enano <zekikpoin@gmail.com>
2020-04-15 11:09:30 -0300

    creamos las funciones createChat y viewAllChats

--- src/controller/chats.js
+++ src/controller/chats.js
@@ -1,26 +1,33 @@
 const chatModel = require('../model/chats')
 const mensajeModel = require('../model/mensajes')
+const userModel = require('../model/users')
 const chatController = {};
 
-chatController.createChat = (req,res)=>{
+chatController.createChat = async (req, res) => {
     /* Creamos un chat */
     //1) Creamos los participantes
-    const participante1 = [{nombre:"ezequiel"}];
-    const participante2 = [{nombre:"juan"}]
-    const participantes = participante1.concat(participante2);
-    //2)Creamos el chat
+    const participante1 = await userModel.findOne({nombre:"ezequiel"});
+    const participante2 = await userModel.findOne({nombre:"juan"});
+    const participantes = [{participante:participante1},{participante:participante2}];
+    // 2)Creamos el chat
     const chat = new chatModel({
-        participantes:participantes,
+        participantes
     });
+    //3) Lo persistimos
     chat.save()
-    .then(res.json({status:"chat created"}));
-
+        .then(res.json({ status: "true" }))
+        .catch((err) => {
+            res.json({
+                status: "error",
+                erro: err.message
+            });
+        });
 }
 //DEV TOOL
-chatController.viewAllChats = async(req,res)=>{
+chatController.viewAllChats = async (req, res) => {
     /*Vemos los chats */
     //1)Pedimos los chats
     const chats = await chatModel.find()
-    .then(response=>{res.json(response)})
+        .then(response => { res.json(response) })
 }
 module.exports = chatController;
