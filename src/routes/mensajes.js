af87cc1ca745fccea9ea22eb6ce08143f418dbbf
Ezequiel Enano <zekikpoin@gmail.com>
2020-04-15 14:21:22 -0300

    agregamos una ruta con el parametro chat y creamos las direcciones de get y post vinculadas con createMensajeEnChat y viewMensajeEnChat

--- src/routes/mensajes.js
+++ src/routes/mensajes.js
@@ -1,8 +1,15 @@
 const {Router} = require('express');
 const router = Router();
 
-const {createMensaje,viewAllMensajes} = require('../controller/mensajes');
+const {createMensaje,viewAllMensajes,viewMensajesEnChat,createMensajesEeChat} = require('../controller/mensajes');
 router.route("/")
     .get(viewAllMensajes)
     .post(createMensaje);
+
+
+router.route("/:chat")
+    //Crea un mensaje dentro de un chat
+    .post(createMensajesEeChat)
+    //Ve los mensajes del chat
+    .get(viewMensajesEnChat)
 module.exports = router;
