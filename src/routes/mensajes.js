8857951e22c384014f09e8f05def11223b3640bf
Ezequiel Enano <zekikpoin@gmail.com>
2020-04-17 12:49:04 -0300

    ruta texto creada

--- src/routes/mensajes.js
+++ src/routes/mensajes.js
@@ -1,7 +1,7 @@
 const {Router} = require('express');
 const router = Router();
 
-const {createMensaje,viewAllMensajes,viewMensajesEnChat,createMensajesEnChat} = require('../controller/mensajes');
+const {createMensaje,viewAllMensajes,viewMensajesEnChat,createMensajesEnChat,getTextoChat} = require('../controller/mensajes');
 router.route("/")
     .get(viewAllMensajes)
     .post(createMensaje);
@@ -12,4 +12,8 @@ router.route("/:chat")
     .post(createMensajesEnChat)
     //Ve los mensajes del chat
     .get(viewMensajesEnChat)
+
+router.route('/texto/:chat')
+    .get(getTextoChat)
+
 module.exports = router;
